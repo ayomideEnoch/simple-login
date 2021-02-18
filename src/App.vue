6a530a1dc9f2aa4b3df55a991b351437538476e1
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-02-18 10:40:39 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex w-full h-screen justify-center items-center bg-gray-400">
+    <div class="h-56 w-96 bg-white rounded-xl shadow-xl">
+      <div class="p-5">
+        <input
+          type="text"
+          placeholder="Email"
+          class="bg-gray-300 p-2 rounded-lg w-full mt-6 focus:outline-none"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          class="bg-gray-300 mt-3 p-2 rounded-lg w-full focus:outline-none"
+        />
+        <div class="flex justify-center">
+          <button
+            class="uppercase bg-purple-900 text-white px-4 py-2 rounded-lg mt-6 focus:outline-none"
+          >
+            sign in
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/style.css'
+
 
 Vue.config.productionTip = false
 
