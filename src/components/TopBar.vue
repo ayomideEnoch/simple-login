fc4f82232fca8193e8a7ff33860d9d3331a16f98
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-03-04 15:16:07 +0100

    Iteration...

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <div class="flex pt-4 justify-center">
-      <div class="bg-white rounded-full shadow-lg p-1.5">
+      <div class="bg-white rounded-full shadow-lg p-1">
         <router-link v-for="nav in navs" :key="nav.index" :to="{ name: nav.route }">
           <button
             @click="active = nav.id"
@@ -29,8 +29,8 @@ export default {
         { title: "Page 1", route: "Page1In", id: 1 },
         { title: "Page 2", route: "Page2In", id: 2 },
         { title: "Page 3", route: "Page3In", id: 3 },
-        { title: "Page 4", route: "Page4In", id: 4 },
-        { title: "Page 5", route: "Page5In", id: 5 },
+        // { title: "Page 4", route: "Page4In", id: 4 },
+        // { title: "Page 5", route: "Page5In", id: 5 },
       ],
     };
   },
--- src/views/Page3/Login.vue
+++ src/views/Page3/Login.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="flex w-full">
-    <div class="w-full h-screen justify-left">
+  <div class="flex">
+    <section class="w-full h-screen items-center justify-left px-10 py-2">
       <div class="flex">
-        <img src="../../assets/logo.png" alt="logo" width="25px" height="30px" />
+        <img src="../../assets/logo.png" alt="logo" width="95px" height="30px" />
         <header class="capitalize text-purple-900 text-sm font-bold">Company Logo</header>
       </div>
-      <div class="mt-3">
-        <p class="text-xs">
+      <div class="mt-3 justify-center items-center bg-gray-200 h-screen">
+        <p class="text-center px-4 py-2 bg-purple-900 text-white font-light text-sm">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eaque
           numquam sequi! Natus nostrum laborum sit fugiat facere enim sunt omnis cum,
           veritatis aspernatur exercitationem mollitia nemo ipsum, laudantium molestiae.
         </p>
       </div>
-    </div>
+    </section>
     <section class="w-1/3">
       <div class="flex w-full h-screen justify-center items-center bg-white">
-        <div class="">aaaa</div>
+        <div class="text-purple-800">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius adipisci error
+          tempore facere esse neque eaque qui ut possimus id ipsa soluta in odit,
+          cupiditate tenetur mollitia omnis voluptates? Suscipit!
+        </div>
       </div>
     </section>
     <section class="w-2/3">
